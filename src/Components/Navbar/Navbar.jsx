be55d10cbf613400e5a0a8fd96eca1d54a7767f1
najim2004 <najim10112004@gmail.com>
2024-06-29 22:52:07 +0600

    bug fixing test 1

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 "use client";
 import navList from "@/Data/navList";
-import Link from "next/link";
 import { useMediaQuery } from "react-responsive";
-// import { scroller } from "react-scroll";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
@@ -11,17 +10,13 @@ const Navbar = () => {
     <div className="nav lg:w-full md:w-full !z-50 h-14 flex items-center md:justify-center lg:justify-center">
       {navList?.map((item, idx) => (
         <Link
-          // onClick={() => {
-          //   scroller.scrollTo(item.to, {
-          //     duration: 700,
-          //     spy: true,
-          //     activeClass: "active",
-          //     offset: !isSmallScreen ? -20 : -100,
-          //     smooth: true,
-          //     containerId: !isSmallScreen ? "scrollable" : false,
-          //   });
-          // }}
-          href={`#${item.to}`}
+          to={item.to}
+          duration={700}
+          spy={true}
+          activeClass={"active"}
+          offset={!isSmallScreen ? -20 : -100}
+          smooth={true}
+          containerId={!isSmallScreen ? "scrollable" : false}
           key={idx}
           className={`text-secondary font-primary font-medium h-full flex items-center justify-center rounded-lg px-5 gap-2 cursor-pointer`}
         >
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "next-themes";
+// import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class">{children}</ThemeProvider>
+        {/* <ThemeProvider attribute="class"> */}
+        {children}
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
