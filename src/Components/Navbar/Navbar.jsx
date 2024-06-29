39e392c70025373e0d50164ee9a426303c431e5c
najim2004 <najim10112004@gmail.com>
2024-06-29 23:07:05 +0600

    bug fixing test 4

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,22 @@
 "use client";
 import navList from "@/Data/navList";
 import Link from "next/link";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
-  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
+  // const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
     <div className="nav lg:w-full md:w-full !z-50 h-14 flex items-center md:justify-center lg:justify-center">
       {navList?.map((item, idx) => (
         <Link
-          href={`#${item.to}`}
-          //     duration: 700,
-          //     spy: true,
-          //     activeClass: "active",
-          //     offset: !isSmallScreen ? -20 : -100,
-          //     smooth: true,
-          //     containerId: !isSmallScreen ? "scrollable" : false,
-
+          to={item.to}
+          duration={700}
+          spy={true}
+          activeClass={"active"}
+          offset={-100}
+          smooth={true}
+          containerId={!isSmallScreen ? "scrollable" : false}
           key={idx}
           className={`text-secondary font-primary font-medium h-full flex items-center justify-center rounded-lg px-5 gap-2 cursor-pointer`}
         >
